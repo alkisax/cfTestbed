1799ad5735afb40a343b8108e4a60bc2770504a1
Alkis <alkisax@gmail.com>
2025-03-29 19:31:24 +0200

    creating simple mongoose models/controllers/routes app

--- node/usersApp/controllers/user.controller.js
+++ node/usersApp/controllers/user.controller.js
@@ -16,3 +16,50 @@ exports.findAll = async (req, res) => {
     console.log("problem in reading users", err);
   }
 }
+
+exports.findOne = async (req, res) => {
+  console.log("find user with specific username")
+  let username = req.params.username
+  try {
+    const result = await User.findOne({ username: username})
+    res.json({
+      status: true,
+      data: result
+    })
+  } catch (err) {
+    console.log("problem in finding user", err)    
+    res.json({
+      status: false,
+      data: err
+    })
+  }
+}
+
+exports.create = async (req,res) => {
+  console.log("Create user");
+  let data = req.body
+  const newUser = new User ({
+    username: data.username,
+    password: data.password,
+    name: data.name,
+    surname: data.surname,
+    email: data.email,
+    address: {
+      area: data.address.area,
+      road: data.address.road
+    }
+  })
+  try {
+    const result = await newUser.save()
+    res.json({
+      status: true,
+      data: result
+    })
+  } catch (err) {
+    console.log("problem in creating user", err)    
+    res.json({
+      status: false,
+      data: err
+    })
+  }
+}
