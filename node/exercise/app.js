ef0372d560daab76573f03ee5994bfbdfa9070e6
Alkis <alkisax@gmail.com>
2025-04-21 18:31:36 +0300

    node exercise finished

--- node/exercise/app.js
+++ node/exercise/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const productRoutes  = require('./routes/product.routes')
+const productRoutes = require('./routes/product.routes')
 
 const app = express()
 
